13ca75833095e7e942318370bf7b892f55331f17
Suranga <surangamunasinghe22568@gmail.com>
2026-08-21 19:16:51 +0530

    add site map

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from "vite-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  sitemap({
+    base: "https://modotravels.com",
+    urls: [
+      "/",
+      "/destinations",
+      "/seasons",
+      "/activities",
+      "/planner",
+      "/contact"
+    ],
+  }),
+  ],
 })
